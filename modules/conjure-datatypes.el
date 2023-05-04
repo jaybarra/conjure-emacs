ea9dee7e6762ef726c479c9d455848fd074f63ac
Jay Barra <jay.barra@nasa.gov>
2023-05-04 16:57:20 -0400

    map kml to nxml-mode

--- modules/conjure-datatypes.el
+++ modules/conjure-datatypes.el
@@ -6,6 +6,7 @@
 (add-to-list 'auto-mode-alist '("\\.echo10\\'" . nxml-mode))
 (add-to-list 'auto-mode-alist '("\\.iso-smap\\'" . nxml-mode))
 (add-to-list 'auto-mode-alist '("\\.iso19115\\'" . nxml-mode))
+(add-to-list 'auto-mode-alist '("\\.kml\\'" . nxml-mode))
 
 ;; Shell Scripts
 (add-to-list 'auto-mode-alist '("\\.zsh\\'" . shell-script-mode))
