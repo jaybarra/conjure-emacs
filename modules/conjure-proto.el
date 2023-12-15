1d00ee9bc2526b04ab9cf1e5e019cebc3a971203
Jay Barra <jay.barra@caylent.com>
2023-12-15 15:31:14 +0000

    Adds a co-pilot module

---
+++ modules/conjure-proto.el
@@ -0,0 +1,9 @@
+;;; conjure-proto.el --- Protobuf configuration
+;;; Commentary:
+;;; Code:
+
+(use-package protobuf-mode)
+
+(provide 'conjure-proto)
+
+;;; conjure-proto.el ends here
--- modules/conjure-python.el
+++ modules/conjure-python.el
@@ -7,11 +7,15 @@
 (use-package exec-path-from-shell)
 (use-package pyvenv-auto)
 
-(use-package python
-  :straight nil
-  :config
-  (exec-path-from-shell-initialize)
-  (setq python-interpreter "ipython"))
+(when (fboundp 'exec-path-from-shell-copy-env)
+  (exec-path-from-shell-copy-env "PYTHONPATH"))
+
+(defun conjure-python-mode-defaults ()
+  "Sensible defaults for Python."
+  (subword-mode +1)
+  )
+
+(setq python-interpreter "ipython")
 
 (add-to-list 'auto-mode-alist '("\\.py\\'" . python-ts-mode))
 (add-hook 'python-ts-mode-hook #'eglot-ensure)
