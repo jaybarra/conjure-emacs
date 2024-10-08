8da2bf24bd55c00c233e32b5a9b0ca169c4f7323
Jay Barra <jay.barra@draconic-systems.com>
2024-10-08 07:44:36 -0400

    Handle java and csharp

---
+++ modules/language/conjure-csharp.el
@@ -0,0 +1,11 @@
+;;; conjure-csharp.el --- Configurations for Csharp in Conjure -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(use-package web-mode
+  :config
+  (add-to-list 'auto-mode-alist '("\\.cshtml?\\'" . web-mode))
+  (setq web-mode-engines-alist '(("razor" . "\\.cshtml\\'"))))
+
+(provide 'conjure-csharp)
+;;; conjure-csharp.el ends here
---
+++ modules/language/conjure-java.el
@@ -0,0 +1,6 @@
+;;; conjure-java.el --- Configurations for Java in Conjure -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(provide 'conjure-java)
+;;; conjure-java.el ends here
